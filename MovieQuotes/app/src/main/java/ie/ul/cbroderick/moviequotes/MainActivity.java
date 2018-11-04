4e4ffe77b6fa94357f18321692eb7bf059779151
Cornelius Broderick <9119124@studentmail.ul.ie>
2018-11-04 13:31:25 +0000

    building ok

--- MovieQuotes/app/src/main/java/ie/ul/cbroderick/moviequotes/MainActivity.java
+++ MovieQuotes/app/src/main/java/ie/ul/cbroderick/moviequotes/MainActivity.java
@@ -5,11 +5,17 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+    private int mTempCounter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +24,26 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        // Temp Firebase tresting are
+        // TODO: Comment out
+        final FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Snackbar.make(view, "Added a Firestore document", Snackbar.LENGTH_LONG).show();
+                // Create a new mq with a first and last name
+                Map<String, Object> mq = new HashMap<>();
+                mTempCounter = mTempCounter + 1;
+                mq.put("quote", "Quote #" + mTempCounter);
+                mq.put("movie", "Movie # " + mTempCounter);
+
+
+                // Add a new document with a generated ID
+                db.collection("moviequotes").add(mq);
             }
         });
     }
