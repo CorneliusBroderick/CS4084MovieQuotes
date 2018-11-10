9619263ae3b7479b4880d717cd425c22e204111b
CorneliusBroderick <9119124@studentmail.ul.ie>
2018-11-10 23:29:17 +0000

    U in CRUD for the detail activity

--- MovieQuotes/app/src/main/java/ie/ul/cbroderick/moviequotes/MovieQuoteDetailActivity.java
+++ MovieQuotes/app/src/main/java/ie/ul/cbroderick/moviequotes/MovieQuoteDetailActivity.java
@@ -1,8 +1,9 @@
 package ie.ul.cbroderick.moviequotes;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -16,6 +17,10 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.annotation.Nullable;
 
@@ -59,10 +64,40 @@ public class MovieQuoteDetailActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                showEditDialog();
+
             }
         });
+    }
+
+    private void showEditDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        View view = getLayoutInflater().inflate(R.layout.moviequote_dialog, null, false );
+        builder.setView(view);
+        builder.setTitle("Edit this quote");
+        final TextView quoteEditText = view.findViewById(R.id.dialog_quote_edittext);
+        final TextView movieEditText = view.findViewById(R.id.dialog_movie_edittext);
+
+        quoteEditText.setText((String)mDocSnapshot.get(Constants.KEY_QUOTE));
+        quoteEditText.setText((String)mDocSnapshot.get(Constants.KEY_MOVIE));
+
+
+        builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                Map<String, Object> mq = new HashMap<>();
+
+                mq.put(Constants.KEY_QUOTE, quoteEditText.getText().toString());
+                mq.put(Constants.KEY_MOVIE, movieEditText.getText().toString());
+                mq.put(Constants.KEY_CREATED, new Date());
+                mDocRef.update(mq);
+            }
+        });
+        builder.setNegativeButton(android.R.string.cancel, null);
+
+
+        builder.create().show();
     }
 
 
