1b2fb827d4885be2b3b489e3a0ef9ac7043f06f9
CorneliusBroderick <9119124@studentmail.ul.ie>
2018-11-10 20:39:23 +0000

    review RecyclerViews

--- MovieQuotes/app/src/main/java/ie/ul/cbroderick/moviequotes/MainActivity.java
+++ MovieQuotes/app/src/main/java/ie/ul/cbroderick/moviequotes/MainActivity.java
@@ -5,11 +5,14 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.support.v7.widget.RecyclerView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -31,6 +34,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+
+        MovieQuoteAdapter movieQuoteAdapter = new MovieQuoteAdapter();
+        recyclerView.setAdapter(movieQuoteAdapter);
+
+
 
         // Temp Firebase tresting are
         // TODO: Comment out
